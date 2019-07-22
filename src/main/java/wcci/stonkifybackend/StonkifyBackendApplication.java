bb58ed69488a0bc9f42dbb47652f2ff5b25f47cd
charles-m-doan <cmatthewdoan@gmail.com>
2019-07-22 18:42:00 -0400

    Initializing first gradle build.

---
+++ src/main/java/wcci/stonkifybackend/StonkifyBackendApplication.java
@@ -0,0 +1,13 @@
+package wcci.stonkifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonkifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonkifyBackendApplication.class, args);
+	}
+
+}
