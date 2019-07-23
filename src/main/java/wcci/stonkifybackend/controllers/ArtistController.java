959e4a463c977aefe722f598173240888900d8d0
charles-m-doan <cmatthewdoan@gmail.com>
2019-07-23 11:49:42 -0400

    Adding CrossOrigin annotation to all controllers.

--- src/main/java/wcci/stonkifybackend/controllers/ArtistController.java
+++ src/main/java/wcci/stonkifybackend/controllers/ArtistController.java
@@ -1,6 +1,7 @@
 package wcci.stonkifybackend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import wcci.stonkifybackend.entities.Artist;
 import wcci.stonkifybackend.repositories.ArtistRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class ArtistController {
--- src/main/java/wcci/stonkifybackend/controllers/SongController.java
+++ src/main/java/wcci/stonkifybackend/controllers/SongController.java
@@ -1,6 +1,7 @@
 package wcci.stonkifybackend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import wcci.stonkifybackend.entities.Song;
 import wcci.stonkifybackend.repositories.SongRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class SongController {
