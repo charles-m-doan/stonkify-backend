b7f16e962152dc4e4f6e51cc288ce3c31ddab438
charles-m-doan <cmatthewdoan@gmail.com>
2019-07-25 09:48:17 -0400

    Adding in Kai's additional code on entities.

--- src/main/java/wcci/stonkifybackend/entities/Album.java
+++ src/main/java/wcci/stonkifybackend/entities/Album.java
@@ -9,133 +9,110 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Album {
-
 	@Id
 	@GeneratedValue
 	private Long id;
-
 	private String title;
 	private String imgUrl;
-
 	private Long parentId;
 	private String parentName;
-
 	private int publishYear;
-
 	@ManyToOne
-	@JsonBackReference
+	@JsonIgnore
 	private Artist artist;
-
 	@JsonManagedReference
 	@OneToMany(mappedBy = "album")
 	private Set<Song> songs = new HashSet<Song>();
 
-	protected Album()
-		{
-		}
+	protected Album() {
+	}
 
-	public Album(String title, String imgUrl, Artist artist, int publishYear)
-		{
+	public Album(String title, String imgUrl, Artist artist, int publishYear) {
 		this.title = title;
 		this.imgUrl = imgUrl;
 		this.artist = artist;
 		this.parentId = artist.getId();
 		this.parentName = artist.getName();
 		this.publishYear = publishYear;
-		}
+	}
 
-	public Album(String title, String imgUrl, Artist artist)
-		{
+	public Album(String title, String imgUrl, Artist artist) {
 		this.title = title;
 		this.imgUrl = imgUrl;
 		this.artist = artist;
 		this.parentId = artist.getId();
 		this.parentName = artist.getName();
 		this.publishYear = 0;
-		}
+	}
 
-	public Album(String title, Artist artist)
-		{
+	public Album(String title, Artist artist) {
 		this.title = title;
 		this.imgUrl = "defaultImg.png";
 		this.artist = artist;
 		this.parentId = artist.getId();
 		this.parentName = artist.getName();
 		this.publishYear = 0;
-		}
+	}
 
-	public void addSong(Song song)
-		{
+	public void addSong(Song song) {
 		this.songs.add(song);
-		}
+	}
 
-	public String toString()
-		{
+	public String toString() {
 		return title + " - " + imgUrl;
-		}
+	}
 
-	public Artist getArtist()
-		{
+	public Artist getArtist() {
 		return artist;
-		}
+	}
 
-	public Set<Song> getSongs()
-		{
+	public Set<Song> getSongs() {
 		return songs;
-		}
+	}
 
-	public String getTitle()
-		{
+	public String getTitle() {
 		return title;
-		}
+	}
 
-	public Long getId()
-		{
+	public Long getId() {
 		return id;
-		}
+	}
 
-	public String getImgUrl()
-		{
+	public String getImgUrl() {
 		return imgUrl;
-		}
+	}
 
-	public Long getParentId()
-		{
+	public Long getParentId() {
 		return parentId;
-		}
+	}
 
-	public String getParentName()
-		{
+	public String getParentName() {
 		return parentName;
-		}
+	}
 
-	public int getPublishYear()
-		{
+	public int getPublishYear() {
 		return publishYear;
-		}
+	}
 
-	public void setArtist(Artist artist)
-		{
+	public void setArtist(Artist artist) {
 		this.artist = artist;
-		}
+	}
 
 	@Override
-	public int hashCode()
-		{
+	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
-		}
+	}
 
 	@Override
-	public boolean equals(Object obj)
-		{
+	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -143,13 +120,11 @@ public class Album {
 		if (getClass() != obj.getClass())
 			return false;
 		Album other = (Album) obj;
-		if (id == null)
-			{
+		if (id == null) {
 			if (other.id != null)
 				return false;
-			}
-		else if (!id.equals(other.id))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
-		}
+	}
 }
--- src/main/java/wcci/stonkifybackend/entities/Song.java
+++ src/main/java/wcci/stonkifybackend/entities/Song.java
@@ -5,102 +5,100 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Song {
-
 	@Id
 	@GeneratedValue
 	private Long id;
-
 	private String title;
 	private String linkUrl;
 	private String duration;
 
+	private String parentAlbumTitle;
+	private String parentArtistName;
 	private Long parentAlbumId;
 	private Long parentArtistId;
-
 	@ManyToOne
-	@JsonBackReference
+	@JsonIgnore
 	private Album album;
 
-	protected Song()
-		{
-		}
+	protected Song() {
+	}
 
-	public Song(String title, String linkUrl, String duration, Album album)
-		{
+	public Song(String title, String linkUrl, String duration, Album album) {
 		this.title = title;
 		this.linkUrl = linkUrl;
 		this.duration = duration;
 		this.album = album;
 		this.parentAlbumId = album.getId();
 		this.parentArtistId = album.getParentId();
-		}
+		this.parentAlbumTitle = album.getTitle();
+		this.parentArtistName = album.getParentName();
+	}
 
-	public Song(String title, Album album)
-		{
+	public Song(String title, Album album) {
 		this.title = title;
 		this.linkUrl = "defaultLink.com";
 		this.duration = "00:00";
 		this.album = album;
 		this.parentAlbumId = album.getId();
 		this.parentArtistId = album.getParentId();
-		}
+		this.parentAlbumTitle = album.getTitle();
+		this.parentArtistName = album.getParentName();
+	}
 
-	public String toString()
-		{
+	public String toString() {
 		return title + " - " + duration + " - " + linkUrl;
-		}
+	}
 
-	public String getTitle()
-		{
+	public String getTitle() {
 		return title;
-		}
+	}
 
-	public String getLinkUrl()
-		{
+	public String getLinkUrl() {
 		return linkUrl;
-		}
+	}
 
-	public String getDuration()
-		{
+	public String getDuration() {
 		return duration;
-		}
+	}
 
-	public Long getId()
-		{
+	public String getParentAlbumTitle() {
+		return parentAlbumTitle;
+	}
+
+	public String getParentArtistName() {
+		return parentArtistName;
+	}
+
+	public Long getId() {
 		return id;
-		}
+	}
 
-	public Album getAlbum()
-		{
+	public Album getAlbum() {
 		return album;
-		}
+	}
 
-	public Long getParentAlbumId()
-		{
+	public Long getParentAlbumId() {
 		return parentAlbumId;
-		}
+	}
 
-	public Long getParentArtistId()
-		{
+	public Long getParentArtistId() {
 		return parentArtistId;
-		}
+	}
 
 	@Override
-	public int hashCode()
-		{
+	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
-		}
+	}
 
 	@Override
-	public boolean equals(Object obj)
-		{
+	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -108,14 +106,11 @@ public class Song {
 		if (getClass() != obj.getClass())
 			return false;
 		Song other = (Song) obj;
-		if (id == null)
-			{
+		if (id == null) {
 			if (other.id != null)
 				return false;
-			}
-		else if (!id.equals(other.id))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
-		}
-
+	}
 }
